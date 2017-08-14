07d09c34d59c950161a4f906d95a57c9e6cdf57d
khadonis <kaan.demirkol@outlook.com>
2017-08-14 18:00:03 +0300

    Galeri bölümü bitirildi.

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
-/* $('.hero-video').height(window.outerHeight + 30); */
+$(document).ready(function () {
+    $('.grid').masonry({
+        itemSelector: '.grid-item'
+      }); 
+});
+//bootstrap modal popup convert to lightbox
+$('a.thumb').click(function (event) {
+    event.preventDefault();
+    var src = $(this).attr('href');
+    var content = $('.modal-body');
+    content.empty();
+    content.html('<img src="' + src + '">');
+    $('.modal-profile').modal('{show:true}');
+  });
