c43c7907ffe1ae5571b67ef2fefede030dc1da64
khadonis <kaan.demirkol@outlook.com>
2017-08-16 19:39:54 +0300

    huzur bölümünün kodlaması yapıldı.

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,20 @@
+/*! Custom JS */
+//detect msie
+if (navigator.appName == 'Microsoft Internet Explorer' || !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1)) {
+  $('html').addClass('ie');
+}
+//masonry
 $(document).ready(function () {
-    $('.grid').masonry({
-        itemSelector: '.grid-item'
-      }); 
+  $('.grid').masonry({
+    itemSelector: '.grid-item'
+  });
 });
 //bootstrap modal popup convert to lightbox
 $('a.thumb').click(function (event) {
-    event.preventDefault();
-    var src = $(this).attr('href');
-    var content = $('.modal-body');
-    content.empty();
-    content.html('<img src="' + src + '">');
-    $('.modal-profile').modal('{show:true}');
-  });
+  event.preventDefault();
+  var src = $(this).attr('href');
+  var content = $('.modal-body');
+  content.empty();
+  content.html('<img src="' + src + '">');
+  $('.modal-profile').modal('{show:true}');
+});
